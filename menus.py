c8980fd4e7217746bf62bb93a3f59ecf724332be
Ana Beatriz Alves <anaallvess020@gmail.com>
2023-08-15 21:05:08 -0300

    criei menu veiculos e seus componentes

--- menus.py
+++ menus.py
@@ -10,3 +10,52 @@ def menu_principal():
 
     return opcao    
 
+def menu_veiculos():
+    print("="*20)
+    print("""
+    [1] - CARRO
+    [2] - MOTO
+    [3] - ÔNIBUS
+    [4] - CAMINHÃO """)
+    print("="*20)
+
+def menu_carro():
+    print("=" * 20)
+    print("""
+        [1] - CADASTRAR NOVO CARRO
+        [2] - LISTAR CARROS
+        [3] - EDITAR CADASTRO DO CARRO
+        [4] - DELETAR CARRO
+        [5] - VOLTAR""")
+    print("=" * 20)
+
+def menu_moto():
+    print("=" * 20)
+    print("""
+        [1] - CADASTRAR NOVO MOTO
+        [2] - LISTAR MOTOS
+        [3] - EDITAR CADASTRO DA MOTO
+        [4] - DELETAR MOTO
+        [5] - VOLTAR""")
+    print("=" * 20)
+
+def menu_onibus():
+    print("=" * 20)
+    print("""
+            [1] - CADASTRAR NOVO ÔNIBUS
+            [2] - LISTAR ÔNIBUS
+            [3] - EDITAR CADASTRO DO ÔNIBUS
+            [4] - DELETAR ÔNIBUS
+            [5] - VOLTAR""")
+    print("=" * 20)
+
+def menu_caminhao():
+    print("=" * 20)
+    print("""
+            [1] - CADASTRAR NOVO CAMINHÃO
+            [2] - LISTAR CAMINHÃO
+            [3] - EDITAR CADASTRO DO CAMINHÃO
+            [4] - DELETAR CAMINHÃO
+            [5] - VOLTAR""")
+    print("=" * 20)
+
