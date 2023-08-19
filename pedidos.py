d4b6f4421528e920c3fe1e9a83b6bf0e99e1010b
Luiz Guilherme <luizgmelo_@hotmail.com>
2023-08-19 14:52:11 -0300

    criei funcao pesquisar pedido

--- pedidos.py
+++ pedidos.py
@@ -1,3 +1,5 @@
+from mesa import *
+
 pedidos = []
 
 def criar_pedido(id, num_mesa, produtos, preco, pagamento, data):
@@ -14,3 +16,15 @@ def criar_pedido(id, num_mesa, produtos, preco, pagamento, data):
 def salvar_pedido(pedido):
     pedidos.append(pedido)
     return True
+
+
+def pesquisar_pedido(mesa):
+    if mesa:
+        for pedido in pedidos:
+            if (mesa["numero"] == pedido["num_mesa"]):
+                return pedido
+        return "Pedido não existe"
+    else:
+        return "Mesa não existe"
+
+
