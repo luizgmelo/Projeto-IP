8f12d2409fe812a8d0991ac93620fb2b0d54ac58
Ana Beatriz Alves <anaallvess020@gmail.com>
2023-08-20 11:59:19 -0300

    Criei menus

---
+++ menus.py
@@ -0,0 +1,123 @@
+def menu_principal():
+    print("="*20)
+    print(""" AUTO ESCOLA
+    [1] - CADASTROS
+    [2] - CONSULTAS
+    [3] - SAIR DO SISTEMA """)
+    print("="*20)
+    
+    opcao = str(input("Opção: "))
+
+    return opcao
+
+def menu_cadastro():
+    while True:
+        print("="*20)
+        print("""
+        [1] - PESSOAS
+        [2] - VEICULOS
+        [3] - HÓRARIOS
+        [4] - VOLTAR  """)
+        print("="*20)
+
+        opcao = str(input("Opção: "))
+
+        if opcao == '1':
+            return menu_pessoas()
+        elif opcao == '2':
+            return menu_veiculos()
+        elif opcao == '3':
+            return menu_horarios()
+        elif opcao == '4':
+            return ''
+        else:
+            print("Opção Inválida, Tente Novamente")
+
+def menu_veiculos():
+    print("="*20)
+    print("""
+    [1] - CARRO
+    [2] - MOTO
+    [3] - ÔNIBUS
+    [4] - CAMINHÃO """)
+    print("="*20)
+
+def menu_carro():
+    print("=" * 20)
+    print("""
+        [1] - CADASTRAR NOVO CARRO
+        [2] - LISTAR CARROS
+        [3] - EDITAR CADASTRO DO CARRO
+        [4] - DELETAR CARRO
+        [5] - VOLTAR""")
+    print("=" * 20)
+
+def menu_moto():
+    print("=" * 20)
+    print("""
+        [1] - CADASTRAR NOVO MOTO
+        [2] - LISTAR MOTOS
+        [3] - EDITAR CADASTRO DA MOTO
+        [4] - DELETAR MOTO
+        [5] - VOLTAR""")
+    print("=" * 20)
+
+def menu_onibus():
+    print("=" * 20)
+    print("""
+            [1] - CADASTRAR NOVO ÔNIBUS
+            [2] - LISTAR ÔNIBUS
+            [3] - EDITAR CADASTRO DO ÔNIBUS
+            [4] - DELETAR ÔNIBUS
+            [5] - VOLTAR""")
+    print("=" * 20)
+
+def menu_caminhao():
+    print("=" * 20)
+    print("""
+            [1] - CADASTRAR NOVO CAMINHÃO
+            [2] - LISTAR CAMINHÃO
+            [3] - EDITAR CADASTRO DO CAMINHÃO
+            [4] - DELETAR CAMINHÃO
+            [5] - VOLTAR""")
+    print("=" * 20)
+
+def menu_pessoas():
+    print("="*20)
+    print("""    [1] - INSTRUTORES
+    [2] - RECEPCIONISTAS
+    [3] - ALUNOS 
+    [4] - VOLTAR """)
+    print("="*20)
+
+
+def menu_instrutores():
+    print("="*20)
+    print("""    [1] - CADASTRAR NOVO INSTRUTOR
+    [2] - LISTAR INSTRUTORES
+    [3] - EDITAR CADASTRO DO INSTRUTOR
+    [4] - DELETAR INSTRUTOR 
+    [5] - VOLTAR """)
+    print("="*20)
+
+
+def menu_recepcionistas():
+    print("="*20)
+    print("""    [1] - CADASTRAR NOVO RECEPCIONISTA
+    [2] - LISTAR RECEPCIONISTAS
+    [3] - EDITAR CADASTRO DO RECEPCIONISTA
+    [4] - DELETAR RECEPCIONISTA 
+    [5] - VOLTAR """)
+    print("="*20)
+
+
+def menu_alunos():
+    print("="*20)
+    print("""    [1] - CADASTRAR NOVO ALUNOS
+    [2] - LISTAR ALUNOS
+    [3] - EDITAR CADASTRO DO ALUNOS
+    [4] - DELETAR ALUNO 
+    [5] - VOLTAR """)
+    print("="*20)
+
+
