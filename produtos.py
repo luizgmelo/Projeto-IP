baff6bf406c90d52683d70c96af5b4104aa34b9f
Ana Beatriz Alves <134332767+anaallves@users.noreply.github.com>
2023-08-21 19:35:59 -0300

    adicionada descricao para criar produto

--- produtos.py
+++ produtos.py
@@ -2,8 +2,8 @@ from menus import menu_editar_produto
 
 produtos = []
 
-def criar_produto(id, nome, tamanho, preco,):
-    return dict(id=id, nome=nome, tamanho=tamanho, preco=preco)
+def criar_produto(id, nome, tamanho, preco, descricao):
+    return dict(id=id, nome=nome, tamanho=tamanho, preco=preco, descricao=descricao)
 
 def salvar_produto(produto):
     produtos.append(produto)
