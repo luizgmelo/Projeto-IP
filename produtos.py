92b3faf4d56ba8cc5a23d5f7c7cd092e4df67bb5
Luiz Guilherme <luizgmelo_@hotmail.com>
2023-08-21 08:52:24 -0300

    criei funcao pesquisar produto

--- produtos.py
+++ produtos.py
@@ -7,4 +7,10 @@ def salvar_produto(produto):
     produtos.append(produto)
     return True
 
+def pesquisar_produto(nome):
+    for produto in produtos:
+        if nome.lower() == produto["nome"].lower():
+            return produto 
+    else:
+        return "Produto não existe.\nVerifique se há erros de digitação.\nDigite o nome do produto completo para a pesquisa ter sucesso."
 
